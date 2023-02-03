cd62cebd9c73aa6186ec708f7c8521e2e3023e51
My Name <cpgsom819@gmail.com>
2023-02-04 01:28:26 +0800

    deposit added

---
+++ deposit.js
@@ -0,0 +1,11 @@
+
+
+document.getElementById('btn-deposit').addEventListener('click', function(){
+
+const depositField =  document.getElementById('deposit filed')
+
+const depositAmount = depositField.value;
+
+console.log(depositAmount);
+
+})
